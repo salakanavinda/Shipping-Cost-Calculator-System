a83c45c0b9de83a3bcfddeced99a24e8fc2dd79b
Salaka Navinda <mmsnavinda@gmail.com>
2020-03-24 11:21:27 +0530

    Fixed Issue By using temporary service

---
+++ back-end/src/main/java/com/Navinda/Customer/Controller/Controller.java
@@ -0,0 +1,57 @@
+package com.Navinda.Customer.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Navinda.Customer.Model.Customer;
+import com.Navinda.Customer.Service.CustomerService;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping(value="/cus-cloud")
+public class Controller {
+
+	@Autowired
+	CustomerService customrService;
+	
+	@RequestMapping(value="/customer", method=RequestMethod.POST)
+	public Customer saveCustomer(@RequestBody Customer customer) {
+		return customrService.save(customer);
+	}
+	
+	@RequestMapping(value="/customer", method=RequestMethod.GET)
+	public List<Customer> getAll(){
+		return customrService.fetch();
+	}
+	
+	@RequestMapping(value="/customer/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Customer> findById(@PathVariable Integer id){
+		if(id<=0) {
+			return ResponseEntity.badRequest().build();
+		}else {
+			Customer customer = customrService.fetch(id);
+			if(customer==null) {
+				return ResponseEntity.notFound().build();
+			}else {
+				return ResponseEntity.ok(customer);
+			}
+		}
+	}
+	//delete customer 
+	@RequestMapping(value="/customer/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<String> deleteCustomer(@PathVariable Integer id) {
+		customrService.deleteCustomerById(id);
+		return new ResponseEntity<>("Customer has been deleted!", HttpStatus.OK);
+	}
+	
+}
+
---
+++ back-end/src/main/java/com/Navinda/Customer/Model/Customer.java
@@ -0,0 +1,49 @@
+package com.Navinda.Customer.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Customer")
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	private Integer age;
+	private boolean active;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	
+	
+
+}
